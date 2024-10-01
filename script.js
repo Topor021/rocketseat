614cfd693a3c3155c296289b7c8fd3fef8d3293a
Topor021 <guilhermeporto023@outlook.com>
2024-10-01 10:34:04 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+
+ /* if (html.classlist.contains("light")) {
+    html.classlist.remove("light")
+  } else {
+    html.classlist.add("light")
+  }*/
+ 
+    html.classList.toggle("light")
+}
